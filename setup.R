57edd91c17601d5cde6a14aad2cc4bc76e0d083c
Daniel Putman <putmands@gmail.com>
2025-04-10 16:02:57 -0400

    add setup file

---
+++ setup.R
@@ -0,0 +1,30 @@
+## update packages
+install.packages("readr")
+install.packages("readxl")
+install.packages("tidyverse")
+install.packages("gtools")
+install.packages("igraph")
+install.packages("RColorBrewer")
+install.packages("pals")
+install.packages("viridis")
+install.packages("ggplot2")
+install.packages("dotwhisker")
+install.packages("multcomp")
+install.packages("patchwork")
+install.packages("lfe")
+install.packages("stargazer")
+install.packages("beepr")
+
+## set up file structure
+home_dir = ~"/radius/"
+setwd(paste0(home_dir,"Colombia/")
+dir.create("Results")
+setwd(paste0(home_dir,"Tanzania/"))
+dir.create("Data")
+dir.create("Tables")
+dir.create("Figures")
+
+
+
+      
+
